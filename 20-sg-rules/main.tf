946b625a204229517250990c59798bc9638e0680
Jayashankar Modili <jayashankar.modili@ibm.com>
2026-06-14 22:34:00 +0530

    hi,

---
+++ 20-sg-rules/main.tf
@@ -0,0 +1,39 @@
+# Bastion access from internet
+resource "aws_security_group_rule" "bastion_internet" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = local.bastion_sg_id
+}
+
+# MongoDB access from Bastion (SSH)
+resource "aws_security_group_rule" "mongodb_bastion" {
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  source_security_group_id = local.bastion_sg_id
+  security_group_id        = local.mongodb_sg_id
+}
+
+# MongoDB access from Catalogue
+resource "aws_security_group_rule" "mongodb_catalogue" {
+  type                     = "ingress"
+  from_port                = 27017
+  to_port                  = 27017
+  protocol                 = "tcp"
+  source_security_group_id = local.catalogue_sg_id
+  security_group_id        = local.mongodb_sg_id
+}
+
+# MongoDB access from User service
+resource "aws_security_group_rule" "mongodb_user" {
+  type                     = "ingress"
+  from_port                = 27017
+  to_port                  = 27017
+  protocol                 = "tcp"
+  source_security_group_id = local.user_sg_id   # ✅ fixed
+  security_group_id        = local.mongodb_sg_id
+}
