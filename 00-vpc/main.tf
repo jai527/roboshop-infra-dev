8c67760ca276db039cc648c775e4e63776d3ec12
Jayashankar Modili <jayashankar.modili@ibm.com>
2026-06-14 20:30:51 +0530

    hi,

--- 00-vpc/main.tf
+++ 00-vpc/main.tf
@@ -3,7 +3,15 @@ module "vpc" {
     project = var.project
     environment = var.environment
     is_peering_required = true
-    
+
 
   
 }
+
+/* module "vpc" {
+    source = "git::https://github.com/jai527/terraform-aws-vpc.git?ref=main"
+    project = var.project
+    environment = var.environment
+    is_peering_required = true
+  
+} */
