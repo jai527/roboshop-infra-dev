9be9d348e408e961a68caa3387ebb7e83dd7be2c
Jayashankar Modili <jayashankar.modili@ibm.com>
2026-06-15 22:19:16 +0530

    all,

--- 30-bastion/bastion.sh
+++ 30-bastion/bastion.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-set -e   # stop if any error
-
-echo "Extending /home disk..."
-
 # Extend partition
 sudo growpart /dev/nvme0n1 4
 
@@ -12,6 +8,6 @@ sudo lvextend -r -L +30G /dev/mapper/rootvg-homevol
 xfs_growfs /home
 echo "✅ /home disk extended successfully"
 
-sudo yum install -y yum-utils
-sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
-sudo yum -y install terraform
+yum install -y yum-utils
+yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+yum -y install terraform
