d0b9b3c4b81c2970e786be97967a59d7ec2ff513
Jayashankar Modili <jayashankar.modili@ibm.com>
2026-06-15 23:03:13 +0530

    all,

--- 40-database/route-53.tf
+++ 40-database/route-53.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "mongodb_r53" {
   zone_id = var.zone_id
-  name    = "mongodb-${var.environment}-${var.domain_name}"
+  name    = "mongodb-${var.environment}"
   type    = "A"
   ttl     = 1
   records = [aws_instance.mongodb.private_ip]
@@ -9,7 +9,7 @@ resource "aws_route53_record" "mongodb_r53" {
 
 resource "aws_route53_record" "redis_r53" {
   zone_id = var.zone_id
-  name    = "redis-${var.environment}-${var.domain_name}"
+  name    = "redis-${var.environment}"
   type    = "A"
   ttl     = 1
   records = [aws_instance.redis.private_ip]
--- 40-database/variables.tf
+++ 40-database/variables.tf
@@ -20,8 +20,3 @@ variable "zone_id" {
     default = "Z005336437MHJ16KCQQI8"
   
 }
-
-variable "domain_name" {
-    default = "jai01.online"
-  
-}
