b4e5fd03cf0e6665f6121101752835d9e0ca7568
Jayashankar Modili <jayashankar.modili@ibm.com>
2026-06-16 23:46:15 +0530

    all,

--- 20-sg-rules/data.tf
+++ 20-sg-rules/data.tf
@@ -27,3 +27,8 @@ data "aws_ssm_parameter" "mysql_sg_id" {
     name = "/${var.project}/${var.environment}/mysql_sg_ids"
   
 }
+
+data "aws_ssm_parameter" "rabbitmq_sg_id" {
+    name = "/${var.project}/${var.environment}/rabbitmq_sg_ids"
+  
+}
