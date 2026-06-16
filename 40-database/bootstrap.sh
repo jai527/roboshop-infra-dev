ad7637d631913ec1d90203bd9b0476b8165cd113
Jayashankar Modili <jayashankar.modili@ibm.com>
2026-06-16 17:47:25 +0530

    all,

--- 40-database/bootstrap.sh
+++ 40-database/bootstrap.sh
@@ -5,7 +5,9 @@ dnf install ansible -y
 
 
 cd /home/ec2-user
+gut pull
 git clone https://github.com/jai527/ansible-roboshop-roles-tf.git
 
 cd ansible-roboshop-roles-tf
+git pull
 ansible-playbook -e component=$component roboshop.yaml
