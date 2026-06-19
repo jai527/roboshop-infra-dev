d47d7bbdd8500749a88281786c532e5f668b8afc
Jayashankar Modili <jayashankar.modili@ibm.com>
2026-06-19 12:55:27 +0530

    hi

--- 60-catalogue/data.tf
+++ 60-catalogue/data.tf
@@ -20,5 +20,5 @@ data "aws_ssm_parameter" "private_subnet_id" {
 }
 
 data "aws_ssm_parameter" "vpc_id" {
- name =  "/${var.project}/${var.environment}/vpc_ids"
+ name =  "/${var.project}/${var.environment}/vpc_id"
 }
