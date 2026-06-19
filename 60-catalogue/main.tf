9824bd4db8cac8d8ffa9718fae05e328e9312722
Jayashankar Modili <jayashankar.modili@ibm.com>
2026-06-19 09:06:44 +0530

    hi

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -43,7 +43,7 @@ resource "terraform_data" "catalogue" {
 
  resource "aws_ec2_instance_state" "catalogue" {
   instance_id = aws_instance.catalogue.id
-  state = "stoped"
+  state = "stopped"
   depends_on = [ terraform_data.catalogue ]
   
 }
