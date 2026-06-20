0a5d3dbe7793eb1bbe8d8ccbe569d6e5676a7d6f
Jayashankar Modili <jayashankar.modili@ibm.com>
2026-06-20 21:39:39 +0530

    hi

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -49,7 +49,8 @@ resource "terraform_data" "catalogue" {
 }
 
 resource "aws_ami_from_instance" "catalogue_ami" {
-  name               = "${var.project}-${var.environment}-catalogue-ami"
+  # roboshop-dev-catalogue-v3-instandid will create name
+  name               = "${var.project}-${var.environment}-catalogue-${var.varsion}-${aws_instance.catalogue.id}"
   source_instance_id = aws_instance.catalogue.id
 
   depends_on = [aws_ec2_instance_state.catalogue]
