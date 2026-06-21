cffad9ccd42b5e7a05f8b9582658781ac08db6f9
Jayashankar Modili <jayashankar.modili@ibm.com>
2026-06-21 16:50:23 +0530

    hi

---
+++ 70-certificate-manager/locals.tf
@@ -0,0 +1,7 @@
+locals {
+  common_tags={
+    project = "roboshop"
+    environment = "Dev"
+    terraform = true
+  }
+}
---
+++ 70-certificate-manager/variables.tf
@@ -0,0 +1,20 @@
+variable "project" {
+    type = string
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+  
+}
+
+variable "zone_id" {
+    default = "Z005336437MHJ16KCQQI8"
+  
+}
+
+
+variable "domain_name" {
+    default = "jai01.online"
+  
+}
