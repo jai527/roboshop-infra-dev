c035f7771ac706906168f11b4491caf8605c6518
Jayashankar Modili <jayashankar.modili@ibm.com>
2026-06-22 23:07:24 +0530

    all

--- 90-component/main.tf
+++ 90-component/main.tf
@@ -4,6 +4,6 @@ for_each = var.components
 source = "git::https://github.com/jai527/terraform-roboshop-component.git?ref=main"
 
 Component = each.key
-rule_priority = each.valve.rule_priority
+rule_priority = each.value.rule_priority
 
 }
