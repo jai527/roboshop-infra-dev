0449a7016bf3e14b48ae9d6facc9286e7866f8bf
Jayashankar Modili <jayashankar.modili@ibm.com>
2026-06-23 11:56:10 +0530

    all

--- 90-component/main.tf
+++ 90-component/main.tf
@@ -1,9 +1,6 @@
-module "Component" {
-for_each = var.components
-
-source = "git::https://github.com/jai527/terraform-roboshop-component.git?ref=main"
-
-Component = each.key
-rule_priority = each.value.rule_priority
-
+module "component" {
+    for_each = var.components
+    source = "git::https://github.com/daws-88s/terraform-roboshop-component.git?ref=main"
+    component = each.key
+    rule_priority = each.value.rule_priority
 }
