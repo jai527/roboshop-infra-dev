fad5cc57787cc1c2e65c8e6b76c18b9e790020ac
Jayashankar Modili <jayashankar.modili@ibm.com>
2026-06-26 16:01:09 +0530

    all

---
+++ 95-vpn/locals.tf
@@ -0,0 +1,11 @@
+locals {
+    common_tags = {
+        project = var.project
+        environment = var.environment
+        terraform = true
+    }
+  public_subnet_id = split( ",", data.aws_ssm_parameter.public_subnet_ids.value)[0]
+  ami_id = data.aws_ami.vpn.id
+  openvpn_sg_id = data.aws_ssm_parameter.openvpn_sg_id.value
+
+}
